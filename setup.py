9fca521b3d37d6bac146087472935930f75fed5b
hilmizul <netspytux@gmail.com>
2020-01-18 05:54:18 +0700

    new version 1.1: fixed platform os

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name = 'alay',
     packages = ['alay'],
-    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
-    description = 'Generate your text to Alay style.',
+    version = '1.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Generate your text into Alay style.',
     author = 'Zul Hilmi',
     author_email = 'netspytux@gmail.com',
     url = 'https://github.com/HilmiZul/alay-generator',
