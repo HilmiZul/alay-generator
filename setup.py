0680d609a40853f2d8aefe802a2740061d47c1f9
hilmizul <netspytux@gmail.com>
2020-01-17 07:31:49 +0700

    create setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'alay',
+    packages = ['alay'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Generate your text to Alay style.',
+    author = 'Zul Hilmi',
+    author_email = 'netspytux@gmail.com',
+    url = 'https://github.com/HilmiZul/alay-generator',
+    download_url = 'https://github.com/HilmiZul/alay-generator/archive/master.zip',
+    keywords = ['alay', 'alay generator', 'bahasa alay'],
+    classifiers = [
+      "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
